55e210d7883e5df5a7ed3502ff5a8a1b6f0f98df
KrystilizeNevaDies <tyreece@rozycki.family>
2024-08-24 15:29:26 +1000

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("java")
     id("io.github.goooler.shadow") version("8.1.7")
-
+    id("maven-publish")
 }
 
+var versionStr = System.getenv("GIT_COMMIT") ?: "dev"
+
 group = "net.mangolise"
-version = "1.0-SNAPSHOT"
+version = versionStr
 
 repositories {
     mavenCentral()
@@ -34,4 +36,36 @@ tasks.withType<Jar> {
 tasks.register("packageWorlds", net.mangolise.gamesdk.gradle.PackageWorldTask::class.java)
 tasks.processResources {
     dependsOn("packageWorlds")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "serbleMaven"
+            url = uri("https://maven.serble.net/snapshots/")
+            credentials {
+                username = System.getenv("SERBLE_REPO_USERNAME")?:""
+                password = System.getenv("SERBLE_REPO_PASSWORD")?:""
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenGitCommit") {
+            groupId = "net.mangolise"
+            artifactId = "paintball"
+            version = versionStr
+            from(components["java"])
+        }
+
+        create<MavenPublication>("mavenLatest") {
+            groupId = "net.mangolise"
+            artifactId = "paintball"
+            version = "latest"
+            from(components["java"])
+        }
+    }
 }
